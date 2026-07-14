4e517521cd7855036b6022e920387ec0030deae8
inseoung <thonam123@naver.com>
2026-07-15 00:06:50 +0900

    shopingmall Project first Commit

---
+++ src/main/java/com/thoany/dst/DstApplication.java
@@ -0,0 +1,13 @@
+package com.thoany.dst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thoany/dst/DstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoany.dst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
